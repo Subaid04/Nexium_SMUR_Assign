1469cd1e90907e55ecd481c7d987d91c4667c2dc
Syed Muhammad Ubaid Ur Rehman <l227730@lhr.nu.edu.pk>
2025-07-16 15:01:21 +0500

    Route fix 2

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,12 @@
 // lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("SUPABASE_ANON_KEY:", process.env.SUPABASE_ANON_KEY?.slice(0, 10));
+
 const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_ANON_KEY!
 );
 
 export default supabase;
-console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
-console.log("SUPABASE_ANON_KEY:", process.env.SUPABASE_ANON_KEY?.slice(0, 10));
